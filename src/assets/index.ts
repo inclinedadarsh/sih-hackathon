1bf8628acda115868ccc5c1195358a33bc2b908e
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-12-23 11:30:03 +0530

    redesign home page with new assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,11 +4,18 @@ import infoImage from "./info.png";
 import mapImage from "./map.png";
 import travelImage from "./travel.png";
 import supplementsImage from "./supplements.png";
+import VRImage from "./vr.png";
 import guide1 from "./guide_1.jpeg";
 import guide2 from "./guide_2.jpeg";
 import guide3 from "./guide_3.jpeg";
 import guide4 from "./guide_4.jpeg";
 import guide5 from "./guide_5.jpeg";
+import tape1 from "./tape1.png";
+import tape2 from "./tape2.png";
+import tape3 from "./tape3.png";
+import godaImage1 from "./goda1.jpeg";
+import godaImage2 from "./goda2.jpeg";
+import godaImage3 from "./goda3.jpeg";
 
 export {
     hotelImage,
@@ -17,9 +24,16 @@ export {
     mapImage,
     travelImage,
     supplementsImage,
+    VRImage,
     guide1,
     guide2,
     guide3,
     guide4,
     guide5,
+    tape1,
+    tape2,
+    tape3,
+    godaImage1,
+    godaImage2,
+    godaImage3,
 };
